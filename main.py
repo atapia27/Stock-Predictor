c66851763cd4a72db1b2aee02f6bad0ed3be92a7
atapia27 <67175024+atapia27@users.noreply.github.com>
2023-02-15 21:44:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@
 
 import csv
 import requests
-
+import pystan as ps
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
-
 from prophet import Prophet
 from prophet.plot import plot_plotly
-
 from plotly import graph_objs as go
 
 
